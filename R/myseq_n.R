f0dcd7be634c1c6da70ed1ab1f3ab4b6969d19f1
Charlie Bollinger <cb8884a@student.american.edu>
2021-05-19 20:09:14 -0400

    function myseq_n created

---
+++ R/myseq_n.R
@@ -0,0 +1,33 @@
+#' myseq_n
+#'
+#' @param x list
+#' @param n factor
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+myseq_n <- function(x, n){
+
+  x1 <- x[c(1)]
+  x2 <- x[c(2)]
+  x3 <- x[c(3)]
+  xn = x3
+  count = 3
+
+  if (n <= 0){
+    stop("enter positive n")
+  } else if (length(x) != 3){
+    stop("enter vector of length 3")
+  } else {
+    while (count < n){
+      xn = x3 + ((x1-x2)/n)
+      x1 = x2
+      x2 = x3
+      x3 = xn
+      count = count + 1
+    }
+    return(xn)
+  }
+}
